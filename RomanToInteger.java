742e5c155029c558aee5467fcb6d7416fbc65cfa
Yagli <jyagkema@amazon.com>
2025-03-28 20:23:43 -0500

    roman to integer - part1

---
+++ RomanToInteger.java
@@ -0,0 +1,28 @@
+//Method called "romanToInteger" returns integer,
+//and takes String "s" as parameter
+
+//The method will return integer version of Roman numeral 
+
+//Questions:
+//Should i handle the situation where "s" is having some symbols different than seven different symbols?
+
+/* 
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+*/
+
+public class RomanToInteger{
+    public static int romanToInteger(String s){
+        int result = 0;
+
+
+
+        return result;
+    }
+}
