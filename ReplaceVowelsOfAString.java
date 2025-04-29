e99d11e4e14c22bb4edf529cd299ccd6b1e9539d
Yagli <jyagkema@amazon.com>
2025-04-28 20:19:10 -0500

    replace vowels of a string

---
+++ ReplaceVowelsOfAString.java
@@ -0,0 +1,37 @@
+// Method called "replaceVowels" returns String
+// and takes String s as a parameter.
+
+// The method will return the input with reversed vowels.
+
+//Questions: Uppercase matters? 
+
+//Examples: 
+/*Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+ */
+
+
+ public class ReplaceVowelsOfAString{
+    public static String replaceVowels(String s){
+        String result;
+        //Create an array of chars so we can reach to the vowels
+        char [] vowels = {'a','e','i','o','u'};
+        //Loop through the string and check if we pass through a vowel.
+        for (int i = 0; i < s.length; i++){
+            
+
+        }
+        //Create a variable that we can log the vowels.
+        //Create the reverse version of that variable.
+
+        //Loop through the string and replace the vowels.
+        return result;
+    }
+ }
